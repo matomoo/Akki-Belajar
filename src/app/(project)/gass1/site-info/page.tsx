b381490261ca3f79d9a35da6be56f427797b8da9
Akki <0374mpl@gmail.com>
2026-02-11 15:42:56 +0800

    feat: fix page gass

--- src/app/(project)/gass1/site-info/page.tsx
+++ src/app/(project)/gass1/site-info/page.tsx
@@ -1,10 +1,8 @@
-import React from "react";
-
 import { FilterBy_Date_SiteId } from "../../gefr/_component/filter-site/filter-by-date-siteid";
 import PageSiteInfo from "../../gefr/_component/gefr/ui-v4/site-info";
 import { TwH3 } from "../../gefr/_component/typography/typography";
 
-export const page = () => {
+const page = () => {
   return (
     <div className="grid grid-cols-1 gap-2 rounded-2xl bg-slate-50 p-4">
       <TwH3 text="Site Info" />
@@ -15,3 +13,5 @@ export const page = () => {
     </div>
   );
 };
+
+export default page;
